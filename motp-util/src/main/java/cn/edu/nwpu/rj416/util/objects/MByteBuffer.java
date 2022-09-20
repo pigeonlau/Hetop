abf804474735fa91bd3ef24331413840b4738dc2
pigeonliu <452418146@qq.com>
2022-09-20 22:10:54 +0800

    version 1.1 fix fix  MByteBuffer construct function size=0 bug 2022/9/20 22:10

--- motp-util/src/main/java/cn/edu/nwpu/rj416/util/objects/MByteBuffer.java
+++ motp-util/src/main/java/cn/edu/nwpu/rj416/util/objects/MByteBuffer.java
@@ -22,6 +22,7 @@ public class MByteBuffer { //32位系统，页表一页大小为4kB
             length *= 2;
         }
         buffer = new byte[length];
+        size = length;
         System.arraycopy(bytes, 0, buffer, 0, len);
     }
 
